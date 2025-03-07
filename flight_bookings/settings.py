99cc5a166ada10a52d4d363cb38006c94693df53
mogr20 <markjordanrandall@gmail.com>
2025-03-07 13:46:20 +0000

    UPDATE: moved and regenerated SECRET_KEY to env file

--- flight_bookings/settings.py
+++ flight_bookings/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+if os.path.isfile('env.py'):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-gsetwkf2&_yl7!@u3xmonj(pa%g*b-h%aauyltv)_+o5!zd=gk'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
