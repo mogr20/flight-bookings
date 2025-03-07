9a7dc729c752f62cba04c7cfb5710f512d951b68
mogr20 <markjordanrandall@gmail.com>
2025-03-07 14:16:42 +0000

    ADD: folder structure for static, and templates. Basic index.html

--- booking/views.py
+++ booking/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 # Create your views here.
+class HomePage(TemplateView):
+    """
+    Displays the home page.
+    """
+    template_name = 'index.html'
