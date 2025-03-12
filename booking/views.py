b7a8411b7f4338b91904df3c41557b39b9feed06
mogr20 <markjordanrandall@gmail.com>
2025-03-12 12:18:08 +0000

    UPDATE: home page displays flight schedule

--- booking/views.py
+++ booking/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
+from django.views import generic
 from django.views.generic import TemplateView
+from .models import Flight
+
 
 # Create your views here.
-class HomePage(TemplateView):
+class HomePage(generic.ListView):
     """
     Displays the home page.
     """
-    template_name = 'index.html'
+    queryset = Flight.objects.all()
+    template_name = 'booking/index.html'
