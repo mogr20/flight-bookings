0012d2e8fb0ea9900d60429c2f1a602db550491e
mogr20 <markjordanrandall@gmail.com>
2025-03-12 14:15:48 +0000

    ADD: Flight detail html, for user to view a specific flight. future functionality to then book that flight

--- booking/urls.py
+++ booking/urls.py
@@ -2,5 +2,6 @@ from .import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.HomePage.as_view(), name='home'),  
+    path('', views.HomePage.as_view(), name='home'), 
+    path('flight/<int:flight_id>/', views.flight_detail, name='flight_detail'),
 ]
--- booking/views.py
+++ booking/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from django.views.generic import TemplateView
 from .models import Flight
@@ -11,3 +11,15 @@ class HomePage(generic.ListView):
     """
     queryset = Flight.objects.all()
     template_name = 'booking/index.html'
+    
+    
+def flight_detail(request, flight_id):
+    """
+    Displays the details of an individual :model:`booking.Flight`.
+    """
+    queryset = Flight.objects.all()
+    flight = get_object_or_404(queryset, pk=flight_id)
+    
+    return render(request, 'booking/flight_detail.html', {
+        "flight": flight,
+    })
