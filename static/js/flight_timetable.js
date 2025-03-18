9b459adc96f58147f57bb44f6ef131b194ef810e
mogr20 <markjordanrandall@gmail.com>
2025-03-18 20:05:11 +0000

    UPDATE: home page uses div instead of table, further info button on tablets and mobiles to fit full text. Needs more styling and flight detail button re-added

---
+++ static/js/flight_timetable.js
@@ -0,0 +1,23 @@
+const expandButtons = document.getElementsByClassName('expand-btn');
+const flightInfoDivs = document.getElementsByClassName('extra-flight-info');
+
+for (let button of expandButtons) {
+    button.addEventListener("click", (e) => {
+        let flightDivId = e.target.getAttribute("data-flight_id");
+        let flightDiv = document.getElementById(flightDivId);
+
+        // If we are clicking a flight info button that is already open, close it
+        if (flightDiv.style.display === "block") {
+            flightDiv.style.display = "none";
+            return;
+        }
+
+        // Hide all flight info divs
+        for (let flight of flightInfoDivs) {
+            flight.style.display = "none";
+        }
+
+        // Display the flight info div associated with the clicked button
+        flightDiv.style.display = "block";
+    });
+}
