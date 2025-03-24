0ded980114e8ae49cf06f9c14985830fe4f0e146
mogr20 <markjordanrandall@gmail.com>
2025-03-24 21:09:16 +0000

    FIX: link colour was inaccessible for login and register pages, changed to red text on the light blue background

--- flight_bookings/settings.py
+++ flight_bookings/settings.py
@@ -99,13 +99,6 @@ WSGI_APPLICATION = 'flight_bookings.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
