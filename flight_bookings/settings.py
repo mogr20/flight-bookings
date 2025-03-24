4278f785077827a3153464c3556489d390132889
mogr20 <markjordanrandall@gmail.com>
2025-03-24 13:43:16 +0000

    FIX: email now required, and form validates

--- flight_bookings/settings.py
+++ flight_bookings/settings.py
@@ -129,6 +129,7 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 ACCOUNT_EMAIL_VERIFICATION = 'none'
+ACCOUNT_EMAIL_REQUIRED = 'True'
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
