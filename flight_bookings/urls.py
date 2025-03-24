22001abcaa25ceaef2b694b64d235c89c72ef565
mogr20 <markjordanrandall@gmail.com>
2025-03-24 11:20:01 +0000

    FIX: my_bookings.html validation changes

--- flight_bookings/urls.py
+++ flight_bookings/urls.py
@@ -20,6 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
-    path('user/', include('booking.urls'), name='user'),
     path('', include('booking.urls'), name='home'),
 ]
